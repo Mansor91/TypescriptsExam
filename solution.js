4655f71ee729673dc1bbee4f8a87b339a3a3777b
Mansor Mansor <mansor.m000@gmail.com>
2021-01-28 11:54:55 +0200

    SOLUTION FOR EXAM IS READY EXECPT OF PART 6 AND 7

--- solution.js
+++ solution.js
@@ -1,60 +1,45 @@
-import { add } from "lodash";
-
 //! (1) Task Class:
 class Task {
-    private _message:string;
-    private _isActive:boolean;
-
-    constructor(message:string) {
+    constructor(message) {
         this._message = message;
         this._isActive = true;
     }
-
     get message() {
         return this._message;
     }
     get isActive() {
         return this._isActive;
     }
-    set isActive(status:boolean) {
+    set isActive(status) {
         this._isActive = status;
     }
 }
-
 //! (2) Impl. + button:
 let addButton = document.getElementById("todo-save");
 addButton.setAttribute("onclick", "addTask()");
-
 function addTask() {
     let taskMes = document.getElementById("todo-item").value;
     let taskList = document.getElementById("todo-list");
-
     let newTask = document.createElement("div");
     newTask.setAttribute("id", `${taskMes}`);
     newTask.className += "todo-item";
-    newTask.style.width = "85%"
+    newTask.style.width = "85%";
     newTask.style.float = "left";
     newTask.innerHTML += taskMes;
-
     //* Create Ok button:
     let okButton = document.createElement("BUTTON");
     okButton.className += "todo-ok";
     addButton.setAttribute("onclick", "signActive()");
     okButton.innerText += "V";
-
     taskList.append(newTask);
     taskList.append(okButton);
-
     //! (3) Save the task in local storage:
     let myTask = new Task(taskMes);
     localStorage.setItem(taskMes, JSON.stringify(myTask));
 }
-
-
 //! (4) Delete all:
 let delAllButton = document.getElementById("todo-delall");
 delAllButton.setAttribute("onclick", "delAllTask()");
-
 function delAllTask() {
     let taskList = document.getElementById("todo-list");
     let taskMes = document.getElementById("todo-item");
@@ -62,3 +47,4 @@ function delAllTask() {
     taskMes.innerText = "";
     localStorage.clear();
 }
+
